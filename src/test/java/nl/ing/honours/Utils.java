07d1cb67fa1b79b9534fc95ba1525046b216f84b
arsleutjes <arwin.sleutjes@gmail.com>
2018-04-12 17:32:01 +0200

    Add tests for POST requests to /categories

---
+++ src/test/java/nl/ing/honours/Utils.java
@@ -0,0 +1,25 @@
+package nl.ing.honours;
+
+import static io.restassured.RestAssured.when;
+
+public class Utils {
+    public static final String BASE_URL = "/api/v1";
+    public static final String CATEGORIES_PATH = "/categories";
+    public static final String TRANSACTIONS_PATH = "/transactions";
+
+    public static final String X_SESSION_ID_HEADER = "X-Session-ID";
+    public static final String APPLICATION_JSON_VALUE = "application/json";
+
+    public static final String EMPTY_BODY = "";
+
+
+    public static String getValidSessionId() {
+        return when().
+                post(BASE_URL + "/sessions").
+                then().assertThat().
+                statusCode(201).
+                contentType(Utils.APPLICATION_JSON_VALUE).
+                extract().
+                path("id");
+    }
+}
--- src/test/java/nl/ing/honours/category/CategoriesTest.java
+++ src/test/java/nl/ing/honours/category/CategoriesTest.java
@@ -1,8 +1,95 @@
 package nl.ing.honours.category;
 
-public class CategoriesTest {
-    public static void main(String[] args) {
+import nl.ing.honours.Utils;
+import org.json.JSONObject;
+import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class CategoriesTest {
+    private static final String GROCERIES_CATEGORY_NAME_VALUE = "Groceries";
+    private static final String ID_NAME = "id";
+    private static final String CATEGORY_NAME = "name";
+    private static final String SOME_ID_VALUE = "0";
+    private static final String CATEGORY_JSON_SCHEMA_PATH = "category-response.json";
+
+    /**
+     * tests for POST /categories
+     * For authentication related tests please see "SessionsTest"
+     */
+    @Test
+    public void testPostWithId() {
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(ID_NAME, SOME_ID_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat().statusCode(405)
+                .contentType(Utils.APPLICATION_JSON_VALUE);
     }
 
+    @Test
+    public void testPostWithName() {
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(matchesJsonSchemaInClasspath(CATEGORY_JSON_SCHEMA_PATH));
+    }
+
+    @Test
+    public void testPostWithEmptyBody() {
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(Utils.EMPTY_BODY)
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat().statusCode(405)
+                .contentType(Utils.APPLICATION_JSON_VALUE);
+    }
+
+    @Test
+    public void testPostWithIdAndName() {
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).put(ID_NAME, SOME_ID_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat().statusCode(405)
+                .contentType(Utils.APPLICATION_JSON_VALUE);
+    }
+
+    @Test
+    public void testPostWithDuplicateName() {
+        String sessionId = Utils.getValidSessionId();
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, sessionId)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH);
+
+        given()
+                .header(Utils.X_SESSION_ID_HEADER, sessionId)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat().statusCode(405)
+                .contentType(Utils.APPLICATION_JSON_VALUE);
+    }
 }
