aa7fb374fcf1f444b7e9d9c2b207e77e8a3edd34
CPlentinger <Casperplentinger@gmail.com>
2018-04-12 14:21:55 +0200

    Start on transactions tests

--- src/test/honours/transactions/TransactionsTest.java
+++ src/test/honours/transactions/TransactionsTest.java
@@ -1,6 +1,38 @@
 package honours.transactions;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 public class TransactionsTest {
 
+    private static final String BASE_URL = "/api/v1";
+    private static final String sessionID = obtainSession();
 
+    @Test
+    public void createInvalidTransaction() {
+        Map<String, Object> requestBody = new HashMap<>();
+        // empty requestBody
+        given()
+                .header("X-session-ID", sessionID).
+        when().
+                post(BASE_URL + "/transactions").
+        then().assertThat().
+                statusCode(405);
+
+
+    }
+
+    private static String obtainSession() {
+        return when().
+                    post(BASE_URL + "/sessions").
+                then().assertThat().
+                    statusCode(201).
+                extract().
+                    path("id");
+    }
 }
