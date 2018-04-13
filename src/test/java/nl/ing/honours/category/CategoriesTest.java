f59a3f334b0fe56e50d77a3e3668117d89bfcc7b
arsleutjes <arwin.sleutjes@gmail.com>
2018-04-13 16:33:22 +0200

    Add some tests for the categories endpoint

--- src/test/java/nl/ing/honours/category/CategoriesTest.java
+++ src/test/java/nl/ing/honours/category/CategoriesTest.java
@@ -1,6 +1,7 @@
 package nl.ing.honours.category;
 
 import nl.ing.honours.Utils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -8,11 +9,61 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class CategoriesTest {
-    private static final String GROCERIES_CATEGORY_NAME_VALUE = "Groceries";
+    private static final String SOME_CATEGORY_NAME_VALUE = "Groceries";
     private static final String ID_NAME = "id";
-    private static final String CATEGORY_NAME = "name";
+    private static final String CATEGORY_NAME_NAME = "name";
     private static final String SOME_ID_VALUE = "0";
     private static final String CATEGORY_JSON_SCHEMA_PATH = "category-response.json";
+
+    /**
+     * tests for GET /categories
+     * For authentication related tests please see "SessionsTest"
+     */
+    @Test
+    public void testGet() {
+        String bodyGet = given()
+                .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .when()
+                .get(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .extract().body().toString();
+        JSONObject jsonObjectGet = new JSONObject(bodyGet);
+        assert jsonObjectGet.getJSONArray(jsonObjectGet.keys().next()).length() == 0;
+    }
+
+    @Test
+    public void testGetAfterPost() {
+        String sessionId = Utils.getValidSessionId();
+        String bodyPost = given()
+                .header(Utils.X_SESSION_ID_HEADER, sessionId)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .body(new JSONObject().put(CATEGORY_NAME_NAME, SOME_CATEGORY_NAME_VALUE).toString())
+                .when()
+                .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .extract().body().toString();
+
+        String bodyGet = given()
+                .header(Utils.X_SESSION_ID_HEADER, sessionId)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .when()
+                .get(Utils.BASE_URL + Utils.CATEGORIES_PATH)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(Utils.APPLICATION_JSON_VALUE)
+                .extract().body().toString();
+        JSONObject jsonObjectPost = new JSONObject(bodyPost);
+        JSONObject jsonObjectGet = new JSONObject(bodyGet);
+        assert jsonObjectGet.getJSONArray(jsonObjectGet.keys().next()).equals(new JSONArray().put(jsonObjectPost));
+    }
 
     /**
      * tests for POST /categories
@@ -36,7 +87,7 @@ public class CategoriesTest {
         given()
                 .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
                 .contentType(Utils.APPLICATION_JSON_VALUE)
-                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .body(new JSONObject().put(CATEGORY_NAME_NAME, SOME_CATEGORY_NAME_VALUE).toString())
                 .when()
                 .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
                 .then()
@@ -64,7 +115,7 @@ public class CategoriesTest {
         given()
                 .header(Utils.X_SESSION_ID_HEADER, Utils.getValidSessionId())
                 .contentType(Utils.APPLICATION_JSON_VALUE)
-                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).put(ID_NAME, SOME_ID_VALUE).toString())
+                .body(new JSONObject().put(CATEGORY_NAME_NAME, SOME_CATEGORY_NAME_VALUE).put(ID_NAME, SOME_ID_VALUE).toString())
                 .when()
                 .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
                 .then()
@@ -78,14 +129,14 @@ public class CategoriesTest {
         given()
                 .header(Utils.X_SESSION_ID_HEADER, sessionId)
                 .contentType(Utils.APPLICATION_JSON_VALUE)
-                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .body(new JSONObject().put(CATEGORY_NAME_NAME, SOME_CATEGORY_NAME_VALUE).toString())
                 .when()
                 .post(Utils.BASE_URL + Utils.CATEGORIES_PATH);
 
         given()
                 .header(Utils.X_SESSION_ID_HEADER, sessionId)
                 .contentType(Utils.APPLICATION_JSON_VALUE)
-                .body(new JSONObject().put(CATEGORY_NAME, GROCERIES_CATEGORY_NAME_VALUE).toString())
+                .body(new JSONObject().put(CATEGORY_NAME_NAME, SOME_CATEGORY_NAME_VALUE).toString())
                 .when()
                 .post(Utils.BASE_URL + Utils.CATEGORIES_PATH)
                 .then()
