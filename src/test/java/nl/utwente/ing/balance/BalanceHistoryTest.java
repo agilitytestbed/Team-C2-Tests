2e0d7aa41bf3f0925f89795c6fe04ec2d8c3a762
CPlentinger <Casperplentinger@gmail.com>
2018-07-13 16:25:43 +0200

    Adds a more complex test for balanceHistory.

--- src/test/java/nl/utwente/ing/balance/BalanceHistoryTest.java
+++ src/test/java/nl/utwente/ing/balance/BalanceHistoryTest.java
@@ -6,10 +6,12 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
 import static io.restassured.RestAssured.given;
+import static java.lang.Math.abs;
 import static nl.utwente.ing.Utils.*;
 import static org.junit.Assert.assertEquals;
 
@@ -72,14 +74,20 @@ public class BalanceHistoryTest {
     public void testExample() {
         String session_id = getValidSessionId();
         System.out.println(session_id);
-        DateTime currentDate = new DateTime();
+        DateTime currentDate = new DateTime().minusDays(1);
         String[] types = new String[]{"deposit", "withdrawal"};
+        int[][] intervalResults = new int[24][4];
         for (int i = 0; i < 24; i++) {
             DateTime newDateTime = currentDate;
             for (int j = 0; j < 4; j++) {
-                newDateTime = newDateTime.minusHours(1);
+                newDateTime = newDateTime.plusHours(1);
                 int amount = new Random().nextInt(100);
                 String type = types[new Random().nextInt(types.length)];
+                if (type.equals("deposit")) {
+                    intervalResults[i][j] = amount;
+                } else {
+                    intervalResults[i][j] = -amount;
+                }
                 JSONObject transactionBody = new JSONObject()
                         .put("date", newDateTime.toDate().getTime())
                         .put("externalIBAN", "NL39RABO0300065264")
@@ -95,6 +103,31 @@ public class BalanceHistoryTest {
                     .statusCode(201);
             }
             currentDate = currentDate.minusMonths(1);
+        }
+        JsonPath jsonHistory = given()
+                .header(X_SESSION_ID_HEADER, session_id)
+                .when()
+                .get(BASE_URL + BALANCE_PATH + "/history")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract().jsonPath();
+        ArrayList<HashMap<String, Object>> history = jsonHistory.get("$");
+        Float lastOpen = 0F;
+        for (int c = 0; c < history.size(); c++) {
+            HashMap<String, Object> candlestick = history.get(c);
+            Float open = (Float) candlestick.get("open");
+            Float expectedVolume = 0F;
+            for (int amount : intervalResults[c]) {
+                expectedVolume += abs(amount);
+                open += amount;
+            }
+            assertEquals(open, candlestick.get("close"));
+            assertEquals(expectedVolume, candlestick.get("volume"));
+            if (c > 0) {
+                assertEquals(lastOpen, candlestick.get("close"));
+            }
+            lastOpen = (Float) candlestick.get("open");
         }
     }
 }
